17f54a8a8c1b9a3b585aeff67f118bbaec53ecdb
Wahyu Tamuu <75367930+whyutams@users.noreply.github.com>
2026-06-29 01:17:53 +0800

    feat: update PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,7 +106,6 @@ export default function RootLayout({
           referrerPolicy="no-referrer"
         />
         {/* PWA Meta & Links */}
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#2C4EEE" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
